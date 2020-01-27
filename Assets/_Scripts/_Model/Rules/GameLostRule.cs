fcd09638aaadc2916f284bc936d464339e65b693
ToprakOzgur <zgrtprk@gmail.com>
2020-01-27 13:26:54 +0300

    game lost added

--- Assets/_Scripts/_Model/Rules/GameLostRule.cs
+++ Assets/_Scripts/_Model/Rules/GameLostRule.cs
@@ -1,11 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameLostRule : IRule
 {
     public RuleResult Validate(Face[,] faceMatrix, List<Face> selectedFaces)
     {
-        return new RuleResult(false, RuleResultIdentifiers.GameLostRuleIdentifier);
+        var pairs = DetectAllPairs(faceMatrix);
+        var result = true;
+
+        foreach (var pair in pairs)
+        {
+            var pathFinder = new PathFinder();
+
+            if (pathFinder.PathFind(faceMatrix, pair))
+                result = false;
+        }
+        Debug.LogError("GAME LOST= " + result);
+        return new RuleResult(result, RuleResultIdentifiers.GameLostRuleIdentifier);
+    }
+
+
+    private List<List<Face>> DetectAllPairs(Face[,] faceMatrix)
+    {
+        var pairsDictionary = new Dictionary<int, List<Face>>();
+
+        //find all pairs
+        foreach (var face in faceMatrix)
+        {
+            if (face == null)
+                continue;
+
+            if (!pairsDictionary.ContainsKey(face.number))
+                pairsDictionary.Add(face.number, new List<Face> { face });
+            else
+            {
+                pairsDictionary[face.number].Add(face);
+            }
+        }
+        var pairs = pairsDictionary.Values.ToList();
+        return pairs;
     }
 }
--- Assets/_Scripts/_Model/Rules/PathFindRule.cs
+++ Assets/_Scripts/_Model/Rules/PathFindRule.cs
@@ -5,16 +5,12 @@ using UnityEngine;
 
 public class PathFindRule : IRule
 {
-
     public RuleResult Validate(Face[,] faceMatrix, List<Face> selectedFaces)
     {
         var pathFinder = new PathFinder();
         var result = pathFinder.PathFind(faceMatrix, selectedFaces);
         return new RuleResult(result, RuleResultIdentifiers.PathFindRuleIdentifier);
     }
-
-
-
 }
 
 
